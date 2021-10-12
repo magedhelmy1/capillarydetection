ee4bad0cdcd197ffbb87bd9f11e5e7c44b7f3623
Maged Helmy <maged.helmy1@outlook.com>
2021-10-12 10:46:21 +0200

    added CI/CD

--- backend/manage.py
+++ backend/manage.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == '__main__':
-    # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
-    # on Azure App Service and should use the production settings.
-    settings_module = "server.production" if 'WEBSITE_HOSTNAME' in os.environ else 'server.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,5 +15,8 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
--- backend/server/wsgi.py
+++ backend/server/wsgi.py
@@ -1,17 +1,16 @@
 """
-WSGI config for server project.
+WSGI config for hello_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-settings_module = "server.production" if 'WEBSITE_HOSTNAME' in os.environ else 'server.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
