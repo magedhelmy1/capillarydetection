829fa10074e8a192acf8aea1a1eb1786469fa00e
Maged Helmy <maged.helmy1@outlook.com>
2021-10-12 10:55:28 +0200

    updated SSH

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]; then
+  echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+  done
 
-    echo "PostgreSQL started"
+  echo "PostgreSQL started"
 fi
 
 exec "$@"
