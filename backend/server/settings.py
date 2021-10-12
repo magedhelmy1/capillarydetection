d6718707260b992db2ffbcd486216c8058f08b41
Maged Helmy <maged.helmy1@outlook.com>
2021-10-12 15:53:44 +0200

    Update settings.py

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -122,11 +122,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 WEBPACK_LOADER = {
     'DEFAULT': {
