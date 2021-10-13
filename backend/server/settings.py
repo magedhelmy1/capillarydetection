cd5d3c0b92f99b602add2482b809eb7d8c0af55f
Maged Helmy <maged.helmy1@outlook.com>
2021-10-13 12:46:51 +0200

    Update settings.py

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
