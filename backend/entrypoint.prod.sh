f072b367ff97c69ce0c2a02137a092bf90cb3ef8
Maged Helmy <maged.helmy1@outlook.com>
2021-10-16 14:30:28 +0200

    Update entrypoint.prod.sh

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -11,9 +11,8 @@ if [ "$DATABASE" = "postgres" ]; then
 fi
 
 # shellcheck disable=SC2164
-cd frontend
-npm install
+(cd frontend && npm install && cd src && npm run prod)
 # shellcheck disable=SC2164
-cd src
-npm run prod
+
+
 exec "$@"
