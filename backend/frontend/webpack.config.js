deee2c43dbece5ef47cd50f1ba2a4b175267e475
Maged Helmy <maged.helmy1@outlook.com>
2021-10-16 13:27:56 +0200

    Update frontend production script

--- backend/frontend/webpack.config.js
+++ backend/frontend/webpack.config.js
@@ -52,5 +52,11 @@ module.exports = {
                 ],
             },
         ]
+    },
+
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
     }
 };
