25ef25467532e4c86b003fcadaf93b662b85acb9
Maged Helmy <maged.helmy1@outlook.com>
2021-10-18 20:41:53 +0200

    test - dynamic fetching of IP address

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -11,6 +11,8 @@ if [ "$DATABASE" = "postgres" ]; then
 fi
 
 (cd frontend && npm install && npm run prod)
+touch .env
+echo "REACT_APP_AXIOS_URL=http://"$REACT_APP_AXIOS_URL"" > .env
 python manage.py collectstatic --noinput
 
 exec "$@"
--- backend/frontend/webpack.config.js
+++ backend/frontend/webpack.config.js
@@ -2,22 +2,6 @@ const Dotenv = require('dotenv-webpack');
 
 module.exports = {
 
-    // resolve: {
-    //     fallback: {
-    //         "os": false,
-    //         "fs": false,
-    //         "tls": false,
-    //         "net": false,
-    //         "path": false,
-    //         "zlib": false,
-    //         "http": false,
-    //         "https": false,
-    //         "stream": false,
-    //         "crypto": false,
-    //         "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify
-    //     }
-    // },
-
     // webpack.config.js
     plugins: [
         new Dotenv()
