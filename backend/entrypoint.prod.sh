3813ee434278cd65490d577e4eaac61f0254d2bc
Maged Helmy <maged.helmy1@outlook.com>
2021-11-04 15:23:09 +0100

    Update entrypoint.prod.sh

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -11,6 +11,9 @@ if [ "$DATABASE" = "postgres" ]; then
 fi
 
 (cd frontend && touch .env && echo "REACT_APP_AXIOS_URL=http://"$REACT_APP_AXIOS_URL"" > .env && npm install && npm run prod)
+python manage.py makemigrations
+python manage.py flush --no-input
+python manage.py migrate
 python manage.py collectstatic --noinput
 
 exec "$@"
