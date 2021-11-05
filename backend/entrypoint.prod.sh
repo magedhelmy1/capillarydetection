52ae0823f615b9533df9779ef1928c91ad151d94
Maged Helmy <maged.helmy1@outlook.com>
2021-11-05 15:16:26 +0100

    Update entrypoint.prod.sh

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -12,7 +12,6 @@ fi
 
 python manage.py makemigrations --no-input
 python manage.py migrate
-(cd frontend && touch .env && echo "REACT_APP_AXIOS_URL=http://"$REACT_APP_AXIOS_URL"" >.env && npm install && npm run prod)
-cd ..
 python manage.py collectstatic --noinput
+(cd frontend && touch .env && echo "REACT_APP_AXIOS_URL=http://"$REACT_APP_AXIOS_URL"" >.env && npm install && npm run prod)
 exec "$@"
