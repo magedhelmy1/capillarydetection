82e8e20c2aa586bdcbfd4198de4ef859ed920a01
Maged Helmy <maged.helmy1@outlook.com>
2021-12-03 14:20:15 +0100

    adding tensorflow

--- frontend_GUI/gui/src/App.js
+++ frontend_GUI/gui/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Classifier from './components/Classifier/Classifier';
-import ImageLIst from './components/ImageList/ImageList';
 import Navigation from './components/Navigation/Navigation';
 import {Route, BrowserRouter, Switch} from 'react-router-dom'
 
@@ -13,7 +12,6 @@ function App() {
             <div className='App'>
                 <Switch>
                     <Route exact path='/' component={Classifier}/>
-                    <Route exact path='/list' component={ImageLIst}/>
                     <Route exact path='*' component={Classifier}/>
                 </Switch>
             </div>
