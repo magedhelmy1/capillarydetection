d0601cd362401b9854d3de15bb61976f31192b47
Maged Helmy <maged.helmy1@outlook.com>
2021-12-09 18:11:28 +0100

    Benchmark Test - Create not part of serializer - 2

--- backend/backend_apps/image_classifier/api/urls.py
+++ backend/backend_apps/image_classifier/api/urls.py
@@ -6,7 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'analyze_im', ImageViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('analyze_im/', process_image, name="process_image"),
     path('task/<task_id>/', get_status, name="get_status"),
 
