a36fc8ff7a7bb900bdddc5832f4e170fa0e79436
Maged Helmy <maged.helmy1@outlook.com>
2021-12-09 19:35:10 +0100

    Update load_testing.py

--- performanceTesting/load_testing.py
+++ performanceTesting/load_testing.py
@@ -4,9 +4,6 @@ from locust import HttpUser, task, constant
 class TestImage(HttpUser):
     host = "http://64.227.106.224/"
 
-    # host = "http://127.0.0.1/"
-
     @task
-    def post_images(self):
-        self.client.post(url="api/analyze_im/")
-
+    def get_users(self):
+        self.client.get(url="api/hello/")
