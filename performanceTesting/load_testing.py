27d7328719ff9ab0a8a33e712e04b939f6b20a62
Maged Helmy <maged.helmy1@outlook.com>
2021-12-10 13:56:50 +0100

    WSGI with Async

--- performanceTesting/load_testing.py
+++ performanceTesting/load_testing.py
@@ -9,5 +9,4 @@ class TestImage(HttpUser):
     def post_images(self):
         # self.client.post(url="api/analyze_im/")
         # self.client.post("api/example/")
-        res = self.client.get(url="api/example/")
-        print(res.content)
+        self.client.get(url="api/example/")
