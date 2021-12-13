2287d197fd1bd3e84e6af88ace3be9393b38b7e1
Maged Helmy <maged.helmy1@outlook.com>
2021-12-13 14:16:26 +0100

    Benchmark ASGI with api/hello/

--- performanceTesting/load_testing.py
+++ performanceTesting/load_testing.py
@@ -3,10 +3,11 @@ from locust import HttpUser, task, constant
 
 class TestImage(HttpUser):
     host = "http://64.227.106.224/"
+
     # host = "http://127.0.0.1/"
 
     @task
     def post_images(self):
         # self.client.get(url="api/example/")
-        self.client.post(url="api/analyze_im/")
-        # self.client.post("api/example/")
+        # self.client.post(url="api/analyze_im/")
+        self.client.post("api/hello/")
