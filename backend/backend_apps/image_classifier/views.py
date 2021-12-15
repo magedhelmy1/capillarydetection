e7953926ddaf47aa80b9b7003f3b29b5b41830fc
Maged Helmy <maged.helmy1@outlook.com>
2021-12-15 16:33:31 +0100

    Adding Async + Celery to frontend

--- backend/backend_apps/image_classifier/views.py
+++ backend/backend_apps/image_classifier/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from rest_framework import status
+from rest_framework.decorators import api_view
+
 from .tasks import algorithm_image
 from django.http import JsonResponse
 from django.shortcuts import render
 import json
+from django.views.decorators.csrf import csrf_protect
+import os
+from django.conf import settings
 
 
 # Experimenting with ASGI
@@ -12,8 +17,8 @@ async def hello(request):
     return HttpResponse("Hello, async Django!")
 
 
-async def example(request):
-    res = await process_image(request)
+async def performance_test(request):
+    res = await performance_test_process_image(request)
 
     json_data = json.loads(res.content)
 
@@ -21,7 +26,7 @@ async def example(request):
                                           "task_status": json_data["task_status"]})
 
 
-async def process_image(request, *args, **kwargs):
+async def performance_test_process_image(request, *args, **kwargs):
     image_name = "test.png"
 
     result = algorithm_image.delay("test", image_name, True)
@@ -29,3 +34,46 @@ async def process_image(request, *args, **kwargs):
     return JsonResponse({"task_id": result.id,
                          "task_status": result.status},
                         status=status.HTTP_200_OK)
+
+
+async def async_image_analyze(request):
+    if request.method == 'POST':
+        print(request.POST)
+
+    # res = await image_algorithm(request)
+    #
+    # json_data = json.loads(res.content)
+    return JsonResponse({"task_id": "ada",
+                         "task_status": "SUCCESS"},
+                        status=status.HTTP_200_OK)
+
+    # return render(request, "index.html", {"task_id": json_data["task_id"],
+    #                                       "task_status": json_data["task_status"]})
+
+
+async def image_algorithm(request, *args, **kwargs):
+    # result = algorithm_image.delay("test", image_name, True)
+
+
+
+        # print(request.POST)
+
+        # value = request.POST.get['picture']
+        # if value.is_valid():
+        #     print(value)
+
+    # image_uploaded = request.data['picture']
+    # image_name = str(request.data['picture'])
+    # file_path = os.path.join(settings.IMAGES_DIR, image_name, )
+    #
+    # with open(file_path, 'wb+') as fp:
+    #     for chunk in image_uploaded:
+    #         fp.write(chunk)
+    #
+    # result = algorithm_image.apply_async((file_path, image_name, False), queue='transient')
+    return JsonResponse({"task_id": "ada",
+                         "task_status": "SUCCESS"},
+                        status=status.HTTP_200_OK)
+    # return JsonResponse({"task_id": result.id,
+    #                      "task_status": result.status},
+    #                     status=status.HTTP_200_OK)
