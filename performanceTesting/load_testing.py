b17b77e1a6ca67019b76f68b5f310d4d3b6213a5
Maged Helmy <maged.helmy1@outlook.com>
2021-12-15 19:02:42 +0100

    switching off TFX

--- performanceTesting/load_testing.py
+++ performanceTesting/load_testing.py
@@ -1,4 +1,5 @@
 from locust import HttpUser, task, constant
+from django.views.decorators.csrf import csrf_exempt
 
 
 class TestImage(HttpUser):
@@ -7,9 +8,10 @@ class TestImage(HttpUser):
     # host = "http://127.0.0.1/"
 
     @task
+    @csrf_exempt
     def post_images(self):
         #self.client.get(url="api/example/")
         # self.client.post(url="api/hello")
         # self.client.post(url="api/analyze_im/")
         # self.client.post("nginx_hello")
-        self.client.get(url="api/performance_test/")
+        self.client.post(url="api/performance_test/")
