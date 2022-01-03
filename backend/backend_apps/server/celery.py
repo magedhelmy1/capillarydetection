9d2edef0cde11fdb8236102ae50305fc5bb4838a
Maged Helmy <maged.helmy1@outlook.com>
2022-01-03 17:56:11 +0100

    removed transient from celery

--- backend/backend_apps/server/celery.py
+++ backend/backend_apps/server/celery.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from kombu import Exchange, Queue
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
@@ -9,13 +8,6 @@ app = Celery('server')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-task_routes = {
-    'proj.tasks.add': {'queue': 'celery', 'delivery_mode': 'transient'}
-}
-
-# worker_prefetch_multiplier = 0
-
 
 # @app.task(bind=True)
 # def debug_task(self):
