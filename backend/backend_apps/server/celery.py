5ad1a41aafbe2622ac870fb3a1c338b36cf331a0
Maged Helmy <maged.helmy1@outlook.com>
2022-01-06 13:09:20 +0100

    Optimizing Celery

--- backend/backend_apps/server/celery.py
+++ backend/backend_apps/server/celery.py
@@ -9,16 +9,13 @@ app = Celery('server')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# Optimization
 worker_send_task_event = False
 task_ignore_result = True
-
-# # task will be killed after 60 seconds
-# task_time_limit = 60
-# # task will raise exception SoftTimeLimitExceeded after 50 seconds
-# task_soft_time_limit = 50
-# # task messages will be acknowledged after the task has been executed, not just before (the default behavior).
-# task_acks_late = True
-# # One worker taks 10 tasks from queue at a time and will increase the performance
+task_time_limit = 60
+task_soft_time_limit = 50
+task_acks_late = True
 worker_prefetch_multiplier = 10
 
 # task_queues = (
