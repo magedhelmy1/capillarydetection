85e4179b815b739cb030c67a84d868b12ad7b0c0
Maged Helmy <maged.helmy1@outlook.com>
2022-01-06 13:03:39 +0100

    Added worker prefetch

--- backend/backend_apps/server/celery.py
+++ backend/backend_apps/server/celery.py
@@ -12,6 +12,15 @@ app.autodiscover_tasks()
 worker_send_task_event = False
 task_ignore_result = True
 
+# # task will be killed after 60 seconds
+# task_time_limit = 60
+# # task will raise exception SoftTimeLimitExceeded after 50 seconds
+# task_soft_time_limit = 50
+# # task messages will be acknowledged after the task has been executed, not just before (the default behavior).
+# task_acks_late = True
+# # One worker taks 10 tasks from queue at a time and will increase the performance
+worker_prefetch_multiplier = 10
+
 # task_queues = (
 #     Queue('celery', routing_key='celery'),
 #     Queue('transient', Exchange('transient', delivery_mode=1),
