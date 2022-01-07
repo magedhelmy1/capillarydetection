117f37560fc7d95660c0952f65a4b31e96c78347
Maged Helmy <maged.helmy1@outlook.com>
2022-01-07 13:58:54 +0100

    switched frontend to api/async_image_analyze

--- frontend_GUI/gui/src/components/Classifier/Classifier.js
+++ frontend_GUI/gui/src/components/Classifier/Classifier.js
@@ -145,8 +145,8 @@ class Classifier extends Component {
         this.activateSpinner()
         let formData = new FormData()
         formData.append('picture', this.state.files.backend_address, this.state.files.name)
-        // axios.post(`${process.env.REACT_APP_AXIOS_URL}/api/async_image_analyze/`, formData, {
-        axios.post(`${process.env.REACT_APP_AXIOS_URL}/api/analyze_im/`, formData, {
+        axios.post(`${process.env.REACT_APP_AXIOS_URL}/api/async_image_analyze/`, formData, {
+        // axios.post(`${process.env.REACT_APP_AXIOS_URL}/api/analyze_im/`, formData, {
             headers: {
                 'accept': 'application/json',
                 'content-type': 'multipart/form-data'
