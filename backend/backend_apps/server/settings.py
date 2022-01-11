99b950229959c951b0ebf79b09e112bb5314b29a
Jyrno Ader <jyrno@thorgate.eu>
2022-01-11 14:10:23 +0200

    Add bucket name

--- backend/backend_apps/server/settings.py
+++ backend/backend_apps/server/settings.py
@@ -233,5 +233,7 @@ if os.environ.get('USE_SPACES', default=''):
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", default="capillarywebspace")
+
     AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", default="fra1")
     AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
