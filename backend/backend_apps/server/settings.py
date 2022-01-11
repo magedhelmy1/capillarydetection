eb25284041d4062c7f81da841ef7bf4a75535fbc
Jyrno Ader <jyrno@thorgate.eu>
2022-01-11 13:40:24 +0200

    Add django-storages and object storage support for files

--- backend/backend_apps/server/settings.py
+++ backend/backend_apps/server/settings.py
@@ -225,3 +225,13 @@ CACHES = {
         }
     }
 }
+
+if os.environ.get('USE_SPACES', default=''):
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+    AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", default="fra1")
+    AWS_S3_ENDPOINT_URL = f"https://${AWS_S3_REGION_NAME}.digitaloceanspaces.com"
